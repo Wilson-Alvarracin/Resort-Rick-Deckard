65350231c3806e0efc97cd4028534c1792a405ac
jovico7 <100007903.joan23@fje.edu>
2023-11-20 19:34:19 +0100

    Comentarios de la base de datos

--- bd_restaurante.sql
+++ bd_restaurante.sql
@@ -13,8 +13,8 @@ CREATE TABLE usuarios (
 CREATE TABLE salas (
     id_sala INT PRIMARY KEY,
     nombre_sala VARCHAR(255),
-    tipo_sala VARCHAR(50),
-    capacidad INT
+    tipo_sala VARCHAR(50),         -- Tipo de sala (Terraza, Comedor, Sala Privada...)
+    capacidad INT                  -- Capacidad de la sala (número de mesas o personas)                    
 );
 
 -- Tabla de mesas 
@@ -22,8 +22,8 @@ CREATE TABLE mesas (
     id_mesa INT PRIMARY KEY,
     numero_mesa INT,
     id_sala INT,
-    estado ENUM('libre','ocupada') DEFAULT ('libre'),
-    FOREIGN KEY (id_sala) REFERENCES salas(id_sala)
+    estado ENUM('libre','ocupada') DEFAULT ('libre'),   -- El estado de la mesa (libre u ocupada)
+    FOREIGN KEY (id_sala) REFERENCES salas(id_sala)     -- Cada mesa está asociada a una sala específica 
 );
 
 -- Tabla para los registros de ocupación de las mesas
@@ -31,10 +31,10 @@ CREATE TABLE ocupaciones (
     id_ocupacion INT PRIMARY KEY,
     id_usuario INT,
     id_mesa INT,
-    fecha_inicio DATETIME DEFAULT CURRENT_TIMESTAMP,
-    fecha_fin DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
-    FOREIGN KEY (id_mesa) REFERENCES mesas(id_mesa)
+    fecha_inicio DATETIME DEFAULT CURRENT_TIMESTAMP,    -- Fecha y hora del inicio de la ocupación
+    fecha_fin DATETIME DEFAULT CURRENT_TIMESTAMP,       -- Fecha y hora del final de la ocupación
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario), -- Permite saber quién ha hecho una ocupación
+    FOREIGN KEY (id_mesa) REFERENCES mesas(id_mesa) -- Permite saber qué mesa ha estado ocupada
 );
 
 
@@ -42,9 +42,9 @@ CREATE TABLE ocupaciones (
 
 -- Insertar usuarios (camareros)
 INSERT INTO usuarios (id_usuario, nombre_user, contrasena) VALUES
-    (1, 'Jorge', '$2y$10$wORRwXyRsJRc9ua8okkNuO6m/GbqBuZouNb4LZbwFPDG6HwNUhOVa'), -- asdASD123
-    (2, 'Olga', '$2y$10$wORRwXyRsJRc9ua8okkNuO6m/GbqBuZouNb4LZbwFPDG6HwNUhOVa'),
-    (3, 'Miguel', '$2y$10$wORRwXyRsJRc9ua8okkNuO6m/GbqBuZouNb4LZbwFPDG6HwNUhOVa');
+    (1, 'Jorge', '$2y$10$wORRwXyRsJRc9ua8okkNuO6m/GbqBuZouNb4LZbwFPDG6HwNUhOVa'),   -- asdASD123
+    (2, 'Olga', '$2y$10$wORRwXyRsJRc9ua8okkNuO6m/GbqBuZouNb4LZbwFPDG6HwNUhOVa'),    -- asdASD123
+    (3, 'Miguel', '$2y$10$wORRwXyRsJRc9ua8okkNuO6m/GbqBuZouNb4LZbwFPDG6HwNUhOVa');  -- asdASD123
 
 -- Insertar salas
 INSERT INTO salas (id_sala, nombre_sala, tipo_sala, capacidad) VALUES
