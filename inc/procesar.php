611986d3d5b61adb3fc40f6cf8901c00a366f59c
Wilson-Alvarracin <100007104.joan23@fje.edu>
2023-11-21 17:53:35 +0100

    version01

--- inc/procesar.php
+++ inc/procesar.php
@@ -1,20 +1,24 @@
 <?php
 include_once('./conexion.php');
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['id_mesa'])) {
+    if (isset($_POST['numero_mesa'])) {
         
-        $sql = "SELECT nombre_mesa FROM mesas WHERE id_mesa = ? ;";
+        $sql = "SELECT numero_mesa FROM mesas WHERE numero_mesa = ? ;";
 
         $stmt = mysqli_prepare($conn, $sql);
-        mysqli_stmt_bind_param($stmt, "i", $id_mesa);
+        mysqli_stmt_bind_param($stmt, "i", $_POST['numero_mesa']);
         mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
+        
+        mysqli_stmt_store_result($stmt);
 
         if (mysqli_stmt_num_rows($stmt) > 0) {
             echo "La mesa con número ".$_POST['numero_mesa']." existe.";
         } else {
             echo "La mesa con número ".$_POST['numero_mesa']." no existe.";
         }
+
+        mysqli_stmt_close($stmt);
         
     } else {
         echo "No funciona";
